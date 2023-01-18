96f70596e6cf1ca56266221c60e1d032f3018351
HeoJY <jyqpwoei@naver.com>
2023-01-18 21:44:59 +0900

    Authority Test Study 1. Spring의 인증과 권한은 FilterSecurityInterceptor와 MethodSecurityInterceptor에서 결정 2. 인증은 FilterChain의 마지막단에 존재, 단독사용 3. 권한은 Global하게 사용하고 각 Controller, Service, Repository에 권한방식을 부여 4. Interceptor에서 권한을 채택하는 방식은 AccessDecisionManager에서 하는데 AffirmativeBased(긍정 위원회)를 주로 택한다

--- AuthorityTest/server/authority-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
+++ AuthorityTest/server/authority-test/src/main/java/com/sp/fc/web/config/SecurityConfig.java
@@ -1,176 +1,67 @@
 package com.sp.fc.web.config;
 
-import com.sp.fc.user.service.SpUserService;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
-import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
-import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.access.vote.AbstractAccessDecisionManager;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.access.AccessDeniedHandlerImpl;
-import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
-import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
-import org.springframework.security.web.session.HttpSessionEventPublisher;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSessionEvent;
-import javax.sql.DataSource;
-import java.time.LocalDateTime;
+import java.util.Collection;
 
 @EnableWebSecurity(debug = true)
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    ExceptionTranslationFilter exceptionTranslationFilter;
-    FilterSecurityInterceptor filterSecurityInterceptor;
-    AccessDeniedHandlerImpl accessDeniedHandler;
-
-    private final SpUserService spUserService;
-    private final DataSource dataSource;
-
-    public SecurityConfig(SpUserService spUserService, DataSource dataSource) {
-        this.spUserService = spUserService;
-        this.dataSource = dataSource;
-    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(spUserService);
+        auth
+                .inMemoryAuthentication()
+                .withUser(
+                        User.withDefaultPasswordEncoder()
+                                .username("user1")
+                                .password("1111")
+                                .roles("USER")
+                );
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Bean
-    RoleHierarchy roleHierarchy(){
-        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
-        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
-        return roleHierarchy;
-    }
-
-    @Bean
-    public ServletListenerRegistrationBean<HttpSessionEventPublisher> httpSessionEventPublisher() {
-        return new ServletListenerRegistrationBean<HttpSessionEventPublisher>(new HttpSessionEventPublisher(){
+    FilterSecurityInterceptor filterSecurityInterceptor;
+    AccessDecisionManager filterAccessDecisionManager(){
+        return new AccessDecisionManager() {
             @Override
-            public void sessionCreated(HttpSessionEvent event) {
-                super.sessionCreated(event);
-                System.out.printf("===>> [%s] 세션 생성됨 %s \n", LocalDateTime.now(), event.getSession().getId());
+            public void decide(Authentication authentication, Object object, Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
+                throw new AccessDeniedException("접근 금지");
+//                return;
             }
 
             @Override
-            public void sessionDestroyed(HttpSessionEvent event) {
-                super.sessionDestroyed(event);
-                System.out.printf("===>> [%s] 세션 만료됨 %s \n", LocalDateTime.now(), event.getSession().getId());
+            public boolean supports(ConfigAttribute attribute) {
+                return true;
             }
 
             @Override
-            public void sessionIdChanged(HttpSessionEvent event, String oldSessionId) {
-                super.sessionIdChanged(event, oldSessionId);
-                System.out.printf("===>> [%s] 세션 아이디 변경  %s:%s \n",  LocalDateTime.now(), oldSessionId, event.getSession().getId());
+            public boolean supports(Class<?> clazz) {
+                return FilterInvocation.class.isAssignableFrom(clazz);
             }
-        });
+        };
     }
-
-    @Bean
-    SessionRegistry sessionRegistry(){
-        SessionRegistryImpl registry = new SessionRegistryImpl();
-        return registry;
-    }
-
-    @Bean
-    PersistentTokenRepository tokenRepository(){
-        JdbcTokenRepositoryImpl repository = new JdbcTokenRepositoryImpl();
-        repository.setDataSource(dataSource);
-        try{
-            repository.removeUserTokens("1");
-        }catch(Exception ex){
-            repository.setCreateTableOnStartup(true);
-        }
-        return repository;
-    }
-
-    @Bean
-    PersistentTokenBasedRememberMeServices rememberMeServices(){
-        PersistentTokenBasedRememberMeServices service =
-                new PersistentTokenBasedRememberMeServices("hello",
-                        spUserService,
-                        tokenRepository()
-                        ){
-                    @Override
-                    protected Authentication createSuccessfulAuthentication(HttpServletRequest request, UserDetails user) {
-                        return new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword(), null);
-//                        return super.createSuccessfulAuthentication(request, user);
-                    }
-                };
-        service.setAlwaysRemember(true);
-        return service;
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests(request->
-                    request
-                            .antMatchers("/", "/error").permitAll()
-                            .antMatchers("/admin/**").hasRole("ADMIN")
-                            .anyRequest().authenticated()
-                )
-                .formLogin(login->
-                        login.loginPage("/login")
-                        .loginProcessingUrl("/loginprocess")
-                        .permitAll()
-                        .defaultSuccessUrl("/", false)
-                        .failureUrl("/login-error")
-                )
-                .logout(logout->
-                        logout.logoutSuccessUrl("/"))
-                .exceptionHandling(error->
-                        error
-//                                .accessDeniedPage("/access-denied")
-                                .accessDeniedHandler(new CustomDeniedHandler())     // 권한 없음
-                                .authenticationEntryPoint(new CustomEntryPoint())   // 인증 실패(로그인 필요)
-
-                )
-                .rememberMe(r->r
-                        .rememberMeServices(rememberMeServices())
-                )
-                .sessionManagement(
-                        s->s
-//                                .sessionCreationPolicy(p-> SessionCreationPolicy.S)
-                                .sessionFixation(sessionFixationConfigurer -> sessionFixationConfigurer.changeSessionId())
-                        .maximumSessions(2)
-                        .maxSessionsPreventsLogin(true)
-                        .expiredUrl("/session-expired")
-                )
-                ;
+                .csrf().disable()
+                .httpBasic().and()
+                .authorizeRequests(
+                        auth->auth
+                                .mvcMatchers("/greeting").hasRole("USER")
+                                .anyRequest().authenticated()
+//                                .accessDecisionManager(filterAccessDecisionManager())
+                );
     }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers("/sessions", "/session/expire", "/session-expired")
-                .requestMatchers(
-                        PathRequest.toStaticResources().atCommonLocations(),
-                        PathRequest.toH2Console()
-                )
-        ;
-    }
-
 }
